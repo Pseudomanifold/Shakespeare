cf6fd94f76f1b40b406b3d5480cbd1cb92f3080e
Bastian Rieck <bastian@rieck.ru>
2016-06-19 15:58:32 +0200

    Attempting CSV format
    Gephi does not like this, for some reason. Labels carry extra
    characters, even though I am escaping them.

--- co-occurrence.py
+++ co-occurrence.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
+import numpy
 import re
 import sys
 
-inScene = False
+inScene         = False
 
 reEnteringScene = r'<SCENE (\d+)>'
 reLeavingScene  = r'</SCENE (\d+)>'
 reSpeakerStart  = r'<([A-Z\d\s]+)>'
 stageDirections = 'STAGE DIR'
 allCharacters   = 'ALL'
+bothCharacters  = 'BOTH' # FIXME: This should become a regular expression
 
+def isSpecialCharacter(name):
+    return    name == stageDirections\
+           or name == allCharacters\
+           or name == bothCharacters
 
 characters = set()
 edges      = set()
@@ -27,18 +33,16 @@ with open(sys.argv[1]) as f:
             inScene = False
         elif inScene and re.match(reSpeakerStart, line):
             character = re.match(reSpeakerStart, line).group(1)
-            if character != stageDirections and character != allCharacters:
+            if not isSpecialCharacter(character):
                 characters.add( character )
 
-#
-# Create basic graph output
-#
+characterIndices = dict()
 
-print("graph G\n"
-      "{\n")
+for index, character in enumerate(sorted(characters)):
+    characterIndices[character] = index
 
-for index,character in enumerate(sorted(characters)):
-    print("%d [label=\"%s\"];" % (index, character))
+n      = len(characters)
+A      = numpy.zeros( (n,n) )
 
 #
 # Extract co-occurences
@@ -56,22 +60,28 @@ with open(sys.argv[1]) as f:
             for character in sorted(charactersInScene):
                 print("  - %s" % character, file=sys.stderr)
 
-            c = list(sorted(characters))
-            i = [ c.index(character) for character in sorted(charactersInScene) ]
-            for u in i:
-                for v in i:
-                    if u < v:
-                        print( "%d -- %d;" % (u,v) )
+            indices = sorted( [ characterIndices[c] for c in charactersInScene ] )
+            for i in range(len(indices)):
+                for j in range(i+1,len(indices)):
+                    u      = indices[i]
+                    v      = indices[j]
+                    A[u,v] = A[u,v] + 1 # Add a connection between the two vertices
+                    A[v,u] = A[u,v]
 
         elif inScene and re.match(reSpeakerStart, line):
             character = re.match(reSpeakerStart, line).group(1)
             # FIXME: Still require special handling for "all" characters within
             # a scene.
-            if character != stageDirections and character != allCharacters:
+            if not isSpecialCharacter(character):
                 charactersInScene.add( character )
 
-#
-# Close graph
-#
+nRows, nColumns = A.shape
+header          = ";"+";".join( ["\""+character+"\"" for character in sorted(characters ) ] )
+characterNames  = sorted(list(characters))
 
-print("}")
+print(header)
+for row in range(nRows):
+    columnStr = "\""+characterNames[row]+"\""
+    for column in range(nColumns):
+        columnStr = columnStr + ";" + str(A[row,column])
+    print(columnStr)
