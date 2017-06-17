681e08bc948defe25e96d929adc49ac3cc3605e8
Bastian Rieck <bastian@rieck.ru>
2017-06-17 20:43:59 +0200

    Better scaling; added threshold support

--- visualize_network.py
+++ visualize_network.py
@@ -13,18 +13,27 @@ import sys
 filename  = sys.argv[1]
 G         = nx.read_pajek(filename)
 positions = nx.drawing.nx_agraph.graphviz_layout(G, prog='neato')
+threshold = float('inf') if len(sys.argv) <= 2 else float(sys.argv[2])
 
 name_to_index = dict()
 for index,name in enumerate(sorted(positions.keys())):
     name_to_index[name] = index
+
+min_pos =  1e6
+max_pos = -1e6
+
+for name in positions.keys():
+    x,y = positions[name]
+    min_pos = min(min_pos, min(x,y))
+    max_pos = max(max_pos, max(x,y))
 
 print("\\begin{tikzpicture}")
 for name in sorted(positions.keys()):
     i   = name_to_index[name]
     x,y = positions[name]
 
-    x /= 100
-    y /= 100
+    x = 5*(x - min_pos) / (max_pos - min_pos)
+    y = 5*(y - min_pos) / (max_pos - min_pos)
 
     print("  \\node (%03d) at (%3.2f,%3.2f) {};" % (i,x,y))
 
@@ -42,10 +51,12 @@ for edge in G.edges(data=True):
     u = name_to_index[source]
     v = name_to_index[target]
     w = data['weight']
-    w = (w - minData)  / (maxData - minData)
-    w = w * 2
 
-    print("  \\draw[line width=%.2fmm] (%03d.center) edge (%03d.center);" % (w,u,v))
+    if w <= threshold:
+        w = (w - minData)  / (maxData - minData)
+        w = w * 2 + 0.10
+
+        print("  \\draw[line width=%.2fmm] (%03d.center) edge (%03d.center);" % (w,u,v))
 
 for name in sorted(positions.keys()):
     i = name_to_index[name]
